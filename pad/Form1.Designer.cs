6a8540b73c088f4d5353c65b02b572298973aa2e
Shun Kashima <x.world.dive@gmail.com>
2021-07-15 18:05:23 +0900

    Add length counter

--- pad/Form1.Designer.cs
+++ pad/Form1.Designer.cs
@@ -37,30 +37,32 @@ namespace pad
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.新規NToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.新しいウィンドウWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.開くOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.上書き保存SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.名前を付けて保存AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.statusStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.textBox1.Location = new System.Drawing.Point(0, 24);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(30);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.textBox1.Size = new System.Drawing.Size(800, 404);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.textBox1_MouseMove);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.statusStrip1.GripMargin = new System.Windows.Forms.Padding(0);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabelStatus,
             this.toolStripStatusBlank,
@@ -73,24 +75,27 @@ namespace pad
             // 
             // toolStripStatusLabelStatus
             // 
+            this.toolStripStatusLabelStatus.ForeColor = System.Drawing.Color.White;
             this.toolStripStatusLabelStatus.Name = "toolStripStatusLabelStatus";
-            this.toolStripStatusLabelStatus.Size = new System.Drawing.Size(39, 17);
-            this.toolStripStatusLabelStatus.Text = "Status";
+            this.toolStripStatusLabelStatus.Size = new System.Drawing.Size(0, 17);
             // 
             // toolStripStatusBlank
             // 
             this.toolStripStatusBlank.Name = "toolStripStatusBlank";
-            this.toolStripStatusBlank.Size = new System.Drawing.Size(709, 17);
+            this.toolStripStatusBlank.Size = new System.Drawing.Size(748, 17);
             this.toolStripStatusBlank.Spring = true;
             // 
             // toolStripStatusLabelCount
             // 
+            this.toolStripStatusLabelCount.ForeColor = System.Drawing.Color.White;
             this.toolStripStatusLabelCount.Name = "toolStripStatusLabelCount";
             this.toolStripStatusLabelCount.Size = new System.Drawing.Size(37, 17);
             this.toolStripStatusLabelCount.Text = "0文字";
             // 
             // menuStrip1
             // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(0);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ファイルFToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
@@ -103,77 +108,67 @@ namespace pad
             // 
             this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.新規NToolStripMenuItem,
-            this.新しいウィンドウWToolStripMenuItem,
             this.開くOToolStripMenuItem,
             this.上書き保存SToolStripMenuItem,
-            this.名前を付けて保存AToolStripMenuItem,
-            this.toolStripSeparator1,
-            this.終了XToolStripMenuItem});
+            this.名前を付けて保存AToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Font = new System.Drawing.Font("Yu Gothic UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ファイルFToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
-            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(68, 20);
             this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
             // 
             // 新規NToolStripMenuItem
             // 
+            this.新規NToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
+            this.新規NToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.新規NToolStripMenuItem.Name = "新規NToolStripMenuItem";
             this.新規NToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.新規NToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.新規NToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
             this.新規NToolStripMenuItem.Text = "新規(&N)";
             this.新規NToolStripMenuItem.Click += new System.EventHandler(this.新規NToolStripMenuItem_Click);
             // 
-            // 新しいウィンドウWToolStripMenuItem
-            // 
-            this.新しいウィンドウWToolStripMenuItem.Name = "新しいウィンドウWToolStripMenuItem";
-            this.新しいウィンドウWToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.N)));
-            this.新しいウィンドウWToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
-            this.新しいウィンドウWToolStripMenuItem.Text = "新しいウィンドウ(&W)";
-            this.新しいウィンドウWToolStripMenuItem.Click += new System.EventHandler(this.新しいウィンドウWToolStripMenuItem_Click);
-            // 
             // 開くOToolStripMenuItem
             // 
+            this.開くOToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
+            this.開くOToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.開くOToolStripMenuItem.Name = "開くOToolStripMenuItem";
             this.開くOToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.開くOToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.開くOToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
             this.開くOToolStripMenuItem.Text = "開く(&O)...";
             this.開くOToolStripMenuItem.Click += new System.EventHandler(this.開くOToolStripMenuItem_Click);
             // 
             // 上書き保存SToolStripMenuItem
             // 
+            this.上書き保存SToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
+            this.上書き保存SToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.上書き保存SToolStripMenuItem.Name = "上書き保存SToolStripMenuItem";
             this.上書き保存SToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.上書き保存SToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.上書き保存SToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
             this.上書き保存SToolStripMenuItem.Text = "上書き保存(&S)";
             this.上書き保存SToolStripMenuItem.Click += new System.EventHandler(this.上書き保存SToolStripMenuItem_Click);
             // 
             // 名前を付けて保存AToolStripMenuItem
             // 
+            this.名前を付けて保存AToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
+            this.名前を付けて保存AToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.名前を付けて保存AToolStripMenuItem.Name = "名前を付けて保存AToolStripMenuItem";
             this.名前を付けて保存AToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
             | System.Windows.Forms.Keys.S)));
-            this.名前を付けて保存AToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.名前を付けて保存AToolStripMenuItem.Size = new System.Drawing.Size(260, 22);
             this.名前を付けて保存AToolStripMenuItem.Text = "名前を付けて保存(&A)...";
             this.名前を付けて保存AToolStripMenuItem.Click += new System.EventHandler(this.名前を付けて保存AToolStripMenuItem_Click);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(254, 6);
-            // 
-            // 終了XToolStripMenuItem
-            // 
-            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
-            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
-            this.終了XToolStripMenuItem.Text = "終了(&X)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.statusStrip1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(204)))), ((int)(((byte)(204)))), ((int)(((byte)(204)))));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Pad";
@@ -194,12 +189,9 @@ namespace pad
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 新規NToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem 新しいウィンドウWToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 開くOToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 上書き保存SToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存AToolStripMenuItem;
-        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
-        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusBlank;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCount;
     }
--- pad/Form1.cs
+++ pad/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace pad
 {
@@ -22,56 +23,115 @@ namespace pad
         private bool _isSaved;
         private string _fileName;
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            this._isSaved = false;
-            string text = string.Format("{0}文字", this.textBox1.Text.Length);
-            this.toolStripStatusLabelCount.Text = text;
-        }
-
         private void 新規NToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (this._isSaved == false)
             {
                 DialogResult result = MessageBox.Show("変更内容を保存しますか？", "Pad", MessageBoxButtons.YesNoCancel);
 
-                if (result == DialogResult.Yes)
+                if (result == DialogResult.Cancel)
+                {
+                    return;
+                }
+                else if (result == DialogResult.Yes)
+                {
+                    if (this._fileName == "")
+                    {
+                        this.SaveAs();
+                    }
+                    else
+                    {
+                        this.Save();
+                    }
+                }
+                else if (result == DialogResult.No)
                 {
 
                 }
+                this._isSaved = true;
+                this._fileName = "";
+                this.textBox1.Text = "";
             }
-        }
-
-        private void 新しいウィンドウWToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void 開くOToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            using var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this._fileName = dialog.FileName;
+                var reader = new StreamReader(this._fileName);
+                this.textBox1.Text = reader.ReadToEnd();
+                reader.Close();
+            }
 
         }
 
-        private void SaveAs(string filename)
+        private void Save()
         {
+            var writer = new StreamWriter(this._fileName);
+            writer.WriteLine(this.textBox1.Text);
+            writer.Close();
+            this._isSaved = true;
+            this.toolStripStatusLabelStatus.Text = "保存しました: " + this._fileName;
+        }
+
+        private void SaveAs()
+        {
+            using var dialog = new SaveFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this._fileName = dialog.FileName;
+                this.Save();
+            }
 
         }
 
         private void 上書き保存SToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            if(this._fileName == "")
+            {
+                this.SaveAs();
+            } 
+            else
+            {
+                this.Save();
+            }
         }
 
         private void 名前を付けて保存AToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            using var dialog = new OpenFileDialog();
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                string filename = dialog.FileName;
-                this.toolStripStatusLabelStatus.Text = filename;
+            this.SaveAs();
+        }
 
+        private void CountLength()
+        {
+            int linage = this.textBox1.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Length;
+            int length = this.textBox1.Text.Length - (2 * (linage - 1));
+            string text = string.Format("{0}文字", length);
+            if (this.textBox1.SelectedText.Length > 0)
+            {
+                int linage_selected = this.textBox1.SelectedText.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).Length;
+                int length_selected = this.textBox1.SelectedText.Length - (2 * (linage_selected - 1));
+                string text_selected = string.Format("{0} / ", length_selected);
+                this.toolStripStatusLabelCount.Text = text_selected + text;
+            }
+            else
+            {
+                this.toolStripStatusLabelCount.Text = text;
             }
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this._isSaved = false;
+            this.CountLength();
+        }
+
+        private void textBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.CountLength();
+        }
     }
 }
+
