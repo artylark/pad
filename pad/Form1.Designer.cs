82fa74813795009f5276e456b8f2742a3a4bb77f
Shun Kashima <x.world.dive@gmail.com>
2021-07-15 16:05:42 +0900

    Create textbox, menubar and statusbar

--- pad/Form1.Designer.cs
+++ pad/Form1.Designer.cs
@@ -29,13 +29,179 @@ namespace pad
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabelStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusBlank = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabelCount = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルFToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新規NToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.新しいウィンドウWToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くOToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.上書き保存SToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.名前を付けて保存AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.終了XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(0, 24);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(30);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(800, 404);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelStatus,
+            this.toolStripStatusBlank,
+            this.toolStripStatusLabelCount});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabelStatus
+            // 
+            this.toolStripStatusLabelStatus.Name = "toolStripStatusLabelStatus";
+            this.toolStripStatusLabelStatus.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabelStatus.Text = "Status";
+            // 
+            // toolStripStatusBlank
+            // 
+            this.toolStripStatusBlank.Name = "toolStripStatusBlank";
+            this.toolStripStatusBlank.Size = new System.Drawing.Size(709, 17);
+            this.toolStripStatusBlank.Spring = true;
+            // 
+            // toolStripStatusLabelCount
+            // 
+            this.toolStripStatusLabelCount.Name = "toolStripStatusLabelCount";
+            this.toolStripStatusLabelCount.Size = new System.Drawing.Size(37, 17);
+            this.toolStripStatusLabelCount.Text = "0文字";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルFToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルFToolStripMenuItem
+            // 
+            this.ファイルFToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新規NToolStripMenuItem,
+            this.新しいウィンドウWToolStripMenuItem,
+            this.開くOToolStripMenuItem,
+            this.上書き保存SToolStripMenuItem,
+            this.名前を付けて保存AToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.終了XToolStripMenuItem});
+            this.ファイルFToolStripMenuItem.Name = "ファイルFToolStripMenuItem";
+            this.ファイルFToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.ファイルFToolStripMenuItem.Text = "ファイル(&F)";
+            // 
+            // 新規NToolStripMenuItem
+            // 
+            this.新規NToolStripMenuItem.Name = "新規NToolStripMenuItem";
+            this.新規NToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.新規NToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.新規NToolStripMenuItem.Text = "新規(&N)";
+            this.新規NToolStripMenuItem.Click += new System.EventHandler(this.新規NToolStripMenuItem_Click);
+            // 
+            // 新しいウィンドウWToolStripMenuItem
+            // 
+            this.新しいウィンドウWToolStripMenuItem.Name = "新しいウィンドウWToolStripMenuItem";
+            this.新しいウィンドウWToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.N)));
+            this.新しいウィンドウWToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.新しいウィンドウWToolStripMenuItem.Text = "新しいウィンドウ(&W)";
+            this.新しいウィンドウWToolStripMenuItem.Click += new System.EventHandler(this.新しいウィンドウWToolStripMenuItem_Click);
+            // 
+            // 開くOToolStripMenuItem
+            // 
+            this.開くOToolStripMenuItem.Name = "開くOToolStripMenuItem";
+            this.開くOToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.開くOToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.開くOToolStripMenuItem.Text = "開く(&O)...";
+            this.開くOToolStripMenuItem.Click += new System.EventHandler(this.開くOToolStripMenuItem_Click);
+            // 
+            // 上書き保存SToolStripMenuItem
+            // 
+            this.上書き保存SToolStripMenuItem.Name = "上書き保存SToolStripMenuItem";
+            this.上書き保存SToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.上書き保存SToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.上書き保存SToolStripMenuItem.Text = "上書き保存(&S)";
+            this.上書き保存SToolStripMenuItem.Click += new System.EventHandler(this.上書き保存SToolStripMenuItem_Click);
+            // 
+            // 名前を付けて保存AToolStripMenuItem
+            // 
+            this.名前を付けて保存AToolStripMenuItem.Name = "名前を付けて保存AToolStripMenuItem";
+            this.名前を付けて保存AToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.名前を付けて保存AToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.名前を付けて保存AToolStripMenuItem.Text = "名前を付けて保存(&A)...";
+            this.名前を付けて保存AToolStripMenuItem.Click += new System.EventHandler(this.名前を付けて保存AToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(254, 6);
+            // 
+            // 終了XToolStripMenuItem
+            // 
+            this.終了XToolStripMenuItem.Name = "終了XToolStripMenuItem";
+            this.終了XToolStripMenuItem.Size = new System.Drawing.Size(257, 22);
+            this.終了XToolStripMenuItem.Text = "終了(&X)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Pad";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelStatus;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルFToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新規NToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 新しいウィンドウWToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くOToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 上書き保存SToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 名前を付けて保存AToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 終了XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusBlank;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCount;
     }
 }
 
--- pad/Form1.cs
+++ pad/Form1.cs
@@ -15,6 +15,62 @@ namespace pad
         public Form1()
         {
             InitializeComponent();
+            this._isSaved = true;
+            this._fileName = "";
+        }
+
+        private bool _isSaved;
+        private string _fileName;
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            this._isSaved = false;
+            string text = string.Format("{0}文字", this.textBox1.Text.Length);
+            this.toolStripStatusLabelCount.Text = text;
+        }
+
+        private void 新規NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (this._isSaved == false)
+            {
+                DialogResult result = MessageBox.Show("変更内容を保存しますか？", "Pad", MessageBoxButtons.YesNoCancel);
+
+                if (result == DialogResult.Yes)
+                {
+
+                }
+            }
+        }
+
+        private void 新しいウィンドウWToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 開くOToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveAs(string filename)
+        {
+
+        }
+
+        private void 上書き保存SToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 名前を付けて保存AToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string filename = dialog.FileName;
+                this.toolStripStatusLabelStatus.Text = filename;
+
+            }
         }
 
     }
